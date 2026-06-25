5c70847ea2b6cdf0e926dcbd0932f67672357e95
daymapavan483 <daymabirdichand483@gmail.com>
2026-06-25 16:09:26 +0530

    other practise at home

--- Clanguage/forvariablepractise.c
+++ Clanguage/forvariablepractise.c
@@ -1,16 +1,34 @@
 #include<stdio.h>
 void main()
 {
-    float comand;
-    rate = 199.90;
-    printf("book price is: %f",rate);
-
     float rate;
     rate = 199.90;
     printf("book price is: %f",rate);
+
+    int age;
+    age = 20;
+    printf("\nbook price is: %d",age);
     
     double price;
     price = 21.2121344;
-    printf("\nvalue of our parts%lf",price);
+    printf("\nvalue of our parts : %lf",price);
+
+
+    int f;
+    f=3;
+    printf("your value is: %d",f);
+    
+    float e;
+    e=4;
+    printf("\nvalue of e %f",e);
+    
+    double a;
+    a=10.888;
+    printf("\nvalue of a %lf",a);
+    
+    long double r;
+    r=10.3443734245432324345436;
+    printf("\nvalue of r %LF",r);
 }
+
 
