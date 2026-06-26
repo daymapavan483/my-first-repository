c7ca066909ec42a30d21ff18945f6cee21b76f57
daymapavan483 <daymabirdichand483@gmail.com>
2026-06-26 18:17:00 +0530

    today we learn about how to take input and give output

---
+++ Clanguage/pro10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    // for character type value there char datatype
+    //for single character means  char datatype
+    
+    char choice;
+    choice = 'h';
+    printf("your choice is %c",choice);
+    
+     char choice2;
+     choice2 = 'f';
+    printf("\nyour second  choice is %c",choice2);
+    
+}
---
+++ Clanguage/pro11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int num1,num2,ans;
+     printf("enter num1: ");
+     scanf("%d",&num1);
+     
+     printf("enter num2 :");
+    scanf("%d",&num2);
+    
+    ans= num1+num2;
+    printf("your sum is %d+%d = %d",num1,num2,ans);
+
+     
+     
+}
